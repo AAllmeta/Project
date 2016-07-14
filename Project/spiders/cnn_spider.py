0e6ef4e32381be869ba5df01c594b7649d66bfe2
MPadilla198 <mp7228757@gmail.com>
2016-07-14 14:17:11 -0500

    Now continuously crawls all of www.cnn.com

--- Project/spiders/cnn_spider.py
+++ Project/spiders/cnn_spider.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Request
 
 from Project.items import CnnItem
 
@@ -12,11 +13,19 @@ class CnnSpider(CrawlSpider):
     start_urls = ['http://www.cnn.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LxmlLinkExtractor(allow=''), callback='parse_item', follow=True),
     )
+
+    def parse_url_start(self, response):
+        print("*************************")
+        requests = []
+        for url in self.start_urls:
+            requests.append(Request(url, callback=self.parse_item))
+        return requests
 
     def parse_item(self, response):
         cnn_item = CnnItem()
         cnn_item["title"] = response.xpath('//title/text()').extract()
-        cnn_item["links"] = response.xpath('//a[@href]/@href').extract()
+        cnn_item["links"] = LxmlLinkExtractor(allow=self.allowed_domains,deny=(),unique=True).extract_links(response)
+
         return cnn_item
